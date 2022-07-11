678093dc9b6335efa5f0dbd8fca59b524b77be01
ItsaMeChun <langtuvotinh1132000@gmail.com>
2022-07-11 13:01:22 +0700

    2

---
+++ src/app/src/main/java/Model/User.java
@@ -0,0 +1,55 @@
+package Model;
+
+public class User {
+    private String Name, emailLogin, hashPassword, Birthday, create_At;
+    private Integer role;
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getEmailLogin() {
+        return emailLogin;
+    }
+
+    public void setEmailLogin(String emailLogin) {
+        this.emailLogin = emailLogin;
+    }
+
+    public String getHashPassword() {
+        return hashPassword;
+    }
+
+    public void setHashPassword(String hashPassword) {
+        this.hashPassword = hashPassword;
+    }
+
+    public String getBirthday() {
+        return Birthday;
+    }
+
+    public void setBirthday(String birthday) {
+        Birthday = birthday;
+    }
+
+    public String getCreate_At() {
+        return create_At;
+    }
+
+    public void setCreate_At(String create_At) {
+        this.create_At = create_At;
+    }
+
+    public Integer getRole() {
+        return role;
+    }
+
+    public void setRole(Integer role) {
+        this.role = role;
+    }
+
+}
