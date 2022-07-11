e08458548befbb543006e1a4b933632a44f4e1c5
ItsaMeChun <langtuvotinh1132000@gmail.com>
2022-07-11 12:52:34 +0700

    first

---
+++ src/app/src/main/java/Helper/helper.java
@@ -0,0 +1,19 @@
+package Helper;
+
+public class helper {
+    public static String localhost = "http://192.168.1.101/model/";
+    public static String registerURL =localhost+ "register.php";
+    public static String loginURL = localhost+  "login.php";
+    /*public static String getAllNews = localhost+  "getNews.php?page=";
+    public static String addCategory = localhost+  "addCategory.php";
+    public static String addNews = localhost+  "addNew.php";
+    public static String addProduct = localhost+  "addProduct.php";
+    public static String removeProduct = localhost+  "removeProductById.php";
+    public static String updateProduct = localhost+  "updateProductById.php";
+    public static String removeCategory = localhost+  "removeCategoryById.php";
+    public static String updateCategory = localhost+  "updateCategoryById.php";
+    public static String removeNews = localhost+  "removeNewsById.php";
+    public static String updateNews = localhost+  "updateNewsById.php";
+    public static String addCatertLogin = localhost+  "addProductLogin.php";*/
+}
+
